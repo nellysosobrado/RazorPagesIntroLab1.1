03989a949da116f105e6e4a0df5f10d6cc329041
nellysosobrado <Nelly.sosobrado@gmail.com>
2025-03-19 11:24:46 +0100

    lab1.1

---
+++ RazorPagesIntroLab1.1/Pages/Champions.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesIntroLab1._1.Pages
+{
+    public class ChampionsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
