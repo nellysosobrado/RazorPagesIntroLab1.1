691a05ba6ebbf4feee35dd8397829264e607f9a7
nellysosobrado <Nelly.sosobrado@gmail.com>
2025-03-19 21:05:15 +0100

    labb 3.3 3.4 New button to display supplier details

--- RazorPagesIntroLab1.1/Pages/SupplierInfo.cshtml.cs
+++ RazorPagesIntroLab1.1/Pages/SupplierInfo.cshtml.cs
@@ -6,14 +6,22 @@ namespace RazorPagesIntroLab1._1.Pages
 {
     public class SupplierInfoModel : PageModel
     {
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string City { get; set; }
+
         private readonly NorthwindContext _dbContext;
 
         public SupplierInfoModel(NorthwindContext dbContext)
         {
             _dbContext = dbContext;
         }
-        public void OnGet()
+        public void OnGet(int suppId) //suppId är en magic string
         {
+            var supplierInfo = _dbContext.Suppliers.First(s =>s.SupplierId == suppId);
+            CompanyName = supplierInfo.CompanyName;
+            ContactName = supplierInfo.ContactName;
+            City = supplierInfo.City;
         }
     }
 }
