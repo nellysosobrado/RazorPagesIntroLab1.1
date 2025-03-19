c3e0a1ae6b7f30cb013f34a0d5e2c3c4e07ed612
nellysosobrado <Nelly.sosobrado@gmail.com>
2025-03-19 11:30:23 +0100

    lab 1.2

--- RazorPagesIntroLab1.1/Pages/Champions.cshtml.cs
+++ RazorPagesIntroLab1.1/Pages/Champions.cshtml.cs
@@ -5,7 +5,15 @@ namespace RazorPagesIntroLab1._1.Pages
 {
     public class ChampionsModel : PageModel
     {
-        public void OnGet()
+        public class ChampionViewModel //View model ett säkert sätt att skicka data till vyn
+                                       //vyn är enbart för att visa data och ska inte innehålla någon logik
+        {
+            public int Id { get; set; } //Id är en unik identifierare för varje champion
+            public string Name { get;set; } //Name är en public string objekt som innehåller namnet på en champion
+            public int Damage { get; set; }
+        }
+        public List<ChampionViewModel> Champions { get; set; } = new();// En lista av ChampionViewModel objekt
+        public void OnGet() //funktion som körs när sidan laddas
         {
         }
     }
