2dbff02756bb9d9218cb991910ed9a6c223ec7c7
nellysosobrado <Nelly.sosobrado@gmail.com>
2025-03-19 12:11:24 +0100

    lab 3.2. Created new Northwind Page, with a button in navbar

---
+++ RazorPagesIntroLab1.1/Pages/NorthWind.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesIntroLab1._1.Pages
+{
+    public class NorthWindModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
